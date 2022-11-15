718d926d9bb01db4551ed8e57427daf634a9fd2c
mathemakitten <helen.ngo14@gmail.com>
2022-11-15 15:13:56 -0800

    world factbook begins

---
+++ cia-world-factbook/run_world_factbook_evaluation.py
@@ -0,0 +1,8 @@
+
+class WorldFactbookEvaluation:
+
+    def __init__(self, data='20220501', device=None, batch_size=16, model='Tristan/olm-bert-base-uncased-oct-2022'):
+        pass
+
+    def run(self):
+        pass
