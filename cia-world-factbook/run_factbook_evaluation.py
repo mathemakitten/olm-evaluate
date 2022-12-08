b3b9ba279e13773579ad7f6b37b1169a41ea7f6b
mathemakitten <helen.ngo14@gmail.com>
2022-12-08 14:51:32 -0800

    push for debugging

--- cia-world-factbook/run_factbook_evaluation.py
+++ cia-world-factbook/run_factbook_evaluation.py
@@ -8,12 +8,14 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 # TODO: turn this into a script usable w fire.Fire
 SNAPSHOT_DATE = '20220901'
 
-device = "cpu"  # "cuda"
+device = "cuda"  # "cuda"
 model_name = "mathemakitten/olm-gpt2-baseline-oct-2022"
-model_name = 'gpt2'
-model_name = 'Tristan/olm-gpt2-oct-2022'
+# model_name = 'gpt2'
+# model_name = 'Tristan/olm-gpt2-oct-2022'
 tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False, use_auth_token=True)
 model = AutoModelForCausalLM.from_pretrained(model_name, use_auth_token=True)
+model = model.to(device)
+print(f"Running eval on {model_name}")
 
 # for gpt2
 if tokenizer.pad_token is None:  # and batch_size > 1:
@@ -30,6 +32,8 @@ def compare_llhs(x):
     previous_line, current_line = x['previous'], x['current']
     tokenized_inputs0 = tokenizer(previous_line, return_tensors="pt", padding=True).to(device=device)
     tokenized_inputs1 = tokenizer(current_line, return_tensors="pt", padding=True).to(device=device)
+    # print("HERE")
+    # print(tokenized_inputs0)
     logits0 = model(**tokenized_inputs0).logits  # .detach().to(device="cpu", dtype=torch.float32)
     logits1 = model(**tokenized_inputs1).logits  # .detach().to(device="cpu", dtype=torch.float32)
     # turn logits into logprobs
@@ -46,8 +50,12 @@ def compare_llhs(x):
         logprobs0.detach().to(device='cpu') / tokenized_inputs0['input_ids'][0].shape[-1],
         logprobs1.detach().to(device='cpu') / tokenized_inputs1['input_ids'][0].shape[-1]
     ]
-    print(f"logprobs: {logprobs}")
-    return num_times_more_current_chosen
+    # print(f"logprobs: {logprobs}")
+    if logprobs[1] > logprobs[0]:
+        num_times_more_current_chosen
+        return 1.0
+    return 0.0
+
 
 with gfile.GFile(f'gs://hugginghelen/olm/factbook/diffs/factbook_diffs_{SNAPSHOT_DATE}_sample.jsonl', 'r') as f:
     data = f.readlines()
@@ -58,14 +66,20 @@ for i, line in enumerate(data):
     x = json.loads(line)
     eval_data.append(x)
 
+import time
+st = time.time()
+print('Running inference')
 for i, x in enumerate(eval_data):
+    if i % 20000 == 0:
+        print(f"Example {i} of {len(eval_data)}")
     total += 1.0  # valid example
     # print(f"\n\nv0: {current_sentence}\nv1: {most_similar_sentence}\nsimilarity: {similarity}")
-    num_times_more_current_chosen = compare_llhs(x)
+    one_if_correct = compare_llhs(x)
+    num_times_more_current_chosen += one_if_correct
 
 print(
     f"How often did this model choose the current one? {num_times_more_current_chosen / total} ({num_times_more_current_chosen} correct, {total} total examples)")
-
+print(f"Took {time.time() - st} s")
 
 
 
