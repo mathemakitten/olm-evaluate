b0cfa175e4525c66664148b272285945fa160e9e
mathemakitten <helen.ngo14@gmail.com>
2022-12-07 20:44:14 -0800

    running but results ???

--- cia-world-factbook/run_factbook_evaluation.py
+++ cia-world-factbook/run_factbook_evaluation.py
@@ -21,14 +21,27 @@ page_ids = [i.split('/')[-1] for i in glob.glob('/home/helen_huggingface_co/wayb
 
 device = None #"cuda"
 model_name = "mathemakitten/olm-gpt2-baseline-oct-2022"
+model_name = 'gpt2'
+model_name = 'Tristan/olm-gpt2-oct-2022'
 tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False, use_auth_token=True)
 model = AutoModelForCausalLM.from_pretrained(model_name, use_auth_token=True)
 
+# for gpt2
+if tokenizer.pad_token is None: # and batch_size > 1:
+    existing_special_tokens = list(tokenizer.special_tokens_map_extended.values())
+    # check that the model already has at least one special token defined
+    # assign one of the special tokens to also be the pad token
+    tokenizer.add_special_tokens({"pad_token": existing_special_tokens[0]})
 
 sentence_embedder = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 
+# These are calculated over the total number of changed examples, NOT documents
+num_times_more_current_chosen, total = 0.0, 0.0
+
 # countries-cambodia has changes for sure between 20220901 and 20220801
-for page in page_ids:
+for page in page_ids[:10]:
+
+    page = 'countries-cambodia'
     files = gfile.glob(f'gs://hugginghelen/olm/factbook/*/{page}/text.txt')
     # files = sorted([os.path.join('gs://hugginghelen/olm/factbook/', d, page, 'text.txt') for d in dates])
 
@@ -59,18 +72,12 @@ for page in page_ids:
 
     # Calculate the normalized summed logprobs for each version, see if current one is more updated
     different_lines = []
-    num_times_more_current_chosen, total = 0.0, 0.0
 
     # TODO: find most-likely-to-be-matched sentence via sentence embedding matrix for this doc to surface most likely corresponding in prev
     for i, l in enumerate(current):
 
-        if not prev:  # sometimes pages are new
+        if not prev:  # sometimes pages are new and have no prior snapshots
             continue
-
-        if i == 3:
-            break
-
-        total += 1
 
         # For this line, find the closest matching line in the previous doc. If none, skip
         curr_embedding = sentence_embedder([l])
@@ -78,21 +85,21 @@ for page in page_ids:
         # print(f"HRRE: {sentences_from_previous}")
         embeddings_for_previous = sentence_embedder(sentences_from_previous)
         likelihoods = tf.matmul(curr_embedding, embeddings_for_previous, transpose_b=True)
+        # Cutoff for most relevant text: 0.5 (picked arbitrarily)
+        similarity = tf.reduce_max(likelihoods[0])  # similarity measurement of most similar
         most_similar_id = sorted(range(len(likelihoods[0])), key=lambda i: likelihoods[0][i], reverse=True)[0]
         most_similar_sentence = prev[most_similar_id]
 
-        # if l != most_similar_sentence:
-        #     print(f"similarity: {most_similar_value}\ncurrent sentence: {l}\nmost likely match: {most_similar_sentence}")
-
         # Assuming that the corresponding previous sentence is not exactly the same
         # TODO: this incorrectly flags cosmetic changes to webpages as real changes
-        if l != most_similar_sentence and len(most_similar_sentence) > 2 and len(curr_embedding) > 2:
+        if l != most_similar_sentence and float(similarity) > 0.5 and float(similarity) < 0.94 and len(most_similar_sentence) > 2 and len(l) > 2:
+            total += 1.0  # valid example only if these conditions are met
 
-            # print(f"lines: {len(l)} - {l}\nlines2: {len(most_similar_sentence)} - {most_similar_sentence}")
+            print(f"\n\nv0: {l}\nv1: {most_similar_sentence}\nsimilarity: {similarity}")
 
             # Get model llhs of both of these and compare
 
-            previous_line, current_line = prev[i], l
+            previous_line, current_line = most_similar_sentence, l
             tokenized_inputs0 = tokenizer(previous_line, return_tensors="pt", padding=True).to(device=device)
             tokenized_inputs1 = tokenizer(current_line, return_tensors="pt", padding=True).to(device=device)
 
@@ -112,12 +119,16 @@ for page in page_ids:
             for t in range(tokenized_inputs1['input_ids'].shape[1] - 1):
                 logprobs1 += logits1[0][t][tokenized_inputs1['input_ids'][0][t+1]].detach().to(device='cpu')
 
-            logprobs = [logprobs0.detach().to(device='cpu'), logprobs1.detach().to(device='cpu')]
+            logprobs = [
+                logprobs0.detach().to(device='cpu') / tokenized_inputs0['input_ids'][0].shape[-1],
+                        logprobs1.detach().to(device='cpu') / tokenized_inputs1['input_ids'][0].shape[-1]
+                        ]
+            print(f"logprobs: {logprobs}")
             # chosen_ending = torch.argmax(torch.Tensor(logprobs)).detach().to(device="cpu")
 
             if logprobs[1] > logprobs[0]:
                 num_times_more_current_chosen += 1.0
 
-
+print(f"How often did this model choose the current one? {num_times_more_current_chosen / total} ({num_times_more_current_chosen} correct, {total} total examples)")
 # For this snapshot, get the diff from the previous month and dump its diff for perplexity calculation
 print('hello')
